3879f2caf04d1813e80e4808543a67a0cbbe0418
nastya <anastasia.i.birillo@gmail.com>
2019-12-02 15:33:46 +0300

    fix

--- services/archive-service.js
+++ services/archive-service.js
@@ -6,12 +6,18 @@ const ARCHIVE_NAME = 'data.zip';
 const intelLogger = require('intel');
 const logger = intelLogger.getLogger('logger');
 
+// const checkExists = async (filePath) => {
+//
+//     filePath = filePath.replace(new RegExp('/(.*):(.*)/uploads/'), APP_DIR)
+//     console.log(filePath);
+// };
+
 const createArchive = (dataItems) => {
     if (fs.existsSync(APP_DIR + ARCHIVE_NAME)){
         fs.unlinkSync(APP_DIR + ARCHIVE_NAME);
     }
 
-    const output = APP_DIR + 'data.zip';
+    const output = APP_DIR + ARCHIVE_NAME;
     const destination = fs.createWriteStream(output);
 
     const archive = archiver('zip', {
@@ -27,13 +33,14 @@ const createArchive = (dataItems) => {
             archive.append(item.codePath, { name: `data${i + 1}.csv` });
             i++;
             logger.info(`${new Date()}: ${item.codePath} was added to archive`);
+          //  checkExists(item.codePath)
         } catch (e) {
             logger.error(`${new Date()}: ${item.codePath} was not added to archive`);
         }
     }
 
     archive.finalize();
-    return '/uploads/data.zip';
+    return `/uploads/${ARCHIVE_NAME}`;
 };
 
 module.exports = {
